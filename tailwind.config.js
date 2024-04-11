f578a42827fb97c685552e17119e7747543a57b6
Pedro Ryan <prgames1234@gmail.com>
2024-04-11 15:37:37 -0300

    fix: fix 'module' is not defined in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -74,4 +75,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
