b99080f298b88a64a14cab6991f72c3a0826c393
Pedro Ryan <prgames1234@gmail.com>
2024-04-11 17:22:04 -0300

    feat: add code mirror editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import CodeMirror, { ViewUpdate } from "@uiw/react-codemirror";
+import { useCallback, useState } from "react";
+
+function Editor() {
+  const [value, setValue] = useState("console.log('hello world!');");
+  const onChange = useCallback((val: string, viewUpdate: ViewUpdate) => {
+    console.log("val:", val);
+    console.log(viewUpdate);
+    setValue(val);
+  }, []);
+
+  return (
+    <div className="overflow-hidden h-full">
+      <CodeMirror
+        value={value}
+        className="h-full"
+        height="100%"
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
 function App() {
-  return <div>The Start</div>;
+  return (
+    <div className="h-screen">
+      <Editor />
+    </div>
+  );
 }
 
 export default App;
