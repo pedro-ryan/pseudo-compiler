9ffa2b2c6b92d713ed87bbedb2f320a598baadce
Pedro Ryan <prgames1234@gmail.com>
2024-04-11 19:00:49 -0300

    style: add transition to console

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,10 @@ import { useCallback, useState } from "react";
 
 function Footer() {
   return (
-    <Collapsible>
+    <Collapsible
+      className="transition-all ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:slide-in-from-bottom"
+      style={{ "--tw-exit-translate-y": "93%" } as React.CSSProperties}
+    >
       <div className="h-7 bg-primary flex justify-center items-center">
         <CollapsibleTrigger asChild>
           <Button variant="ghost" className="text-white h-5 w-2/3 bg-secondary">
@@ -18,7 +21,7 @@ function Footer() {
           </Button>
         </CollapsibleTrigger>
       </div>
-      <CollapsibleContent className="animate-in">
+      <CollapsibleContent className="transition-all ease-in-out data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=closed]:slide-out-to-bottom">
         <div className="bg-muted h-[50vh] transition-all p-4 overflow-auto">
           <p className="text-destructive">
             Uncaught Error: React.Children.only expected to receive a single
