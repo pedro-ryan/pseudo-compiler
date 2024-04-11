9d7eedf1ac610bcacc52819edd219c73452e44ae
Pedro Ryan <prgames1234@gmail.com>
2024-04-11 17:14:45 -0300

    feat: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/components/theme-provider.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -5,6 +6,8 @@ import "./globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark">
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
