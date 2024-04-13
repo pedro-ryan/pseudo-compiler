a62a696a00e7a3bdae9cdff1f0faa76c9347808d
Pedro Ryan <prgames1234@gmail.com>
2024-04-13 11:19:10 -0300

    refactor: remove unnecessary code in compiler

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -3,13 +3,6 @@ import { transform } from "@/compiler/transform";
 import { lexer, parser } from "./parser";
 import "./parsers/loadParsers";
 
-const example_code = `// Oia já tem até comentário
-Algoritmo hello_world
-Inicio
-  Escreva "Hello World!"
-Fim
-`;
-
 export const Logger = (log: unknown) => {
   if (typeof log === "object") {
     console.log(JSON.stringify(log, null, 2));
@@ -19,29 +12,11 @@ export const Logger = (log: unknown) => {
   // TODO log in integrated console
 };
 
-function runner(code: string) {
+export function runner(code: string) {
   const codeRunner = new Function(code);
   codeRunner.call({ Logger });
 }
 
-console.log("\nCODE:");
-Logger(example_code);
-
-console.log("\nLEXER:");
-const LEXER = lexer(example_code);
-Logger(LEXER);
-
-console.log("\nPARSER:");
-const PARSER = parser(LEXER);
-Logger(PARSER);
-
-console.log("\nTRANSFORM:");
-const TRANSFORM = transform(PARSER);
-Logger(TRANSFORM);
-
-console.log("\nGENERATOR:");
-const GENERATOR = generator(TRANSFORM);
-Logger(GENERATOR);
-
-console.log("\nRUNNING:");
-runner(GENERATOR!);
+export function compile(code: string) {
+  return generator(transform(parser(lexer(code))));
+}
