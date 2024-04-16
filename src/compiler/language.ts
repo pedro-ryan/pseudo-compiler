fb556b41043e48db917eddf73feebea5eb2ea5d8
Pedro Ryan <prgames1234@gmail.com>
2024-04-16 18:10:25 -0300

    feat: add autocomplete to the language extension

--- src/compiler/language.ts
+++ src/compiler/language.ts
@@ -1,5 +1,88 @@
 import { parser } from "@/compiler/parser";
+import {
+  Completion,
+  CompletionContext,
+  CompletionResult,
+} from "@codemirror/autocomplete";
+import { LRLanguage, LanguageSupport, syntaxTree } from "@codemirror/language";
+import { SyntaxNode, SyntaxNodeRef } from "@lezer/common";
 import { styleTags, tags as t } from "@lezer/highlight";
+
+function isCompletionContext(
+  context: SyntaxNode | CompletionContext
+): context is CompletionContext {
+  return (context as CompletionContext).pos !== undefined;
+}
+
+function getBefore(context: SyntaxNode | CompletionContext) {
+  if (isCompletionContext(context)) {
+    return syntaxTree(context.state).resolveInner(context.pos, -1);
+  }
+  return context.resolveInner(context.from, -1);
+}
+
+function AutoComplete(context: CompletionContext): CompletionResult | null {
+  const nodeBefore = getBefore(context);
+  const actualNode = syntaxTree(context.state).resolveInner(context.pos, 0);
+  const word = context.matchBefore(/\w*/);
+
+  console.log(word);
+  console.log(actualNode.name);
+
+  if (!word || (word.from == word.to && !context.explicit)) return null;
+
+  let options: Completion[] = [];
+
+  if (
+    nodeBefore.name === "AlgorithmFile" ||
+    (word.text && "Algoritmo".includes(word.text))
+  ) {
+    options.push({
+      label: "Algoritmo",
+      type: "keyword",
+      info: "Palavra-chave utilizada para iniciar a estrutura de um algoritmo deve ser precedia de um nome em snake_case, por exemplo: Algoritmo exemplo_de_nome",
+    });
+  }
+
+  if (actualNode.name === "BlockAlgoritmo") {
+    options = [
+      {
+        label: "Escreva",
+      },
+    ];
+  }
+
+  if (
+    actualNode.name !== "BlockAlgoritmo" &&
+    (nodeBefore.name === "AlgoritmoDeclaration" ||
+      getBefore(nodeBefore).name === "AlgoritmoDeclaration")
+  ) {
+    options = options.concat([
+      {
+        label: "Inicio",
+        type: "keyword",
+      },
+      {
+        label: "Fim",
+        type: "keyword",
+      },
+      {
+        label: "Var",
+        type: "keyword",
+      },
+      {
+        label: "Bloco Algoritmo",
+        type: "text",
+        apply: "Inicio\n\nFim",
+      },
+    ]);
+  }
+
+  return {
+    from: word.from,
+    options,
+  };
+}
 
 const parserWithMetadata = parser.configure({
   props: [
@@ -19,6 +102,7 @@ const AlgorithmLanguage = new LanguageSupport(
     parser: parserWithMetadata,
     languageData: {
       commentTokens: { line: "//" },
+      autocomplete: AutoComplete,
     },
   }),
 );
