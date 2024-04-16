1b6dd3671c158187215a7c228964146741380533
Pedro Ryan <prgames1234@gmail.com>
2024-04-16 18:10:46 -0300

    feat: add linter to the language extension

--- src/compiler/language.ts
+++ src/compiler/language.ts
@@ -5,6 +5,7 @@ import {
   CompletionResult,
 } from "@codemirror/autocomplete";
 import { LRLanguage, LanguageSupport, syntaxTree } from "@codemirror/language";
+import { Diagnostic, linter } from "@codemirror/lint";
 import { SyntaxNode, SyntaxNodeRef } from "@lezer/common";
 import { styleTags, tags as t } from "@lezer/highlight";
 
@@ -84,6 +85,71 @@ function AutoComplete(context: CompletionContext): CompletionResult | null {
   };
 }
 
+const Linter = linter((view) => {
+  const diagnostics: Diagnostic[] = [];
+
+  const logError = (
+    node: SyntaxNodeRef,
+    msg: string,
+    overwrite?: Partial<Diagnostic>
+  ) => {
+    diagnostics.push({
+      from: node.from,
+      to: node.to,
+      severity: "error",
+      message: "Error: " + msg,
+      ...overwrite,
+    });
+  };
+
+  // let nodeBefore = "";
+  let indentation = 0;
+
+  syntaxTree(view.state)
+    .cursor()
+    .iterate(
+      (node) => {
+        // const next = node.node.nextSibling?.name;
+        // console.table([
+        //   ["last", "Enter", "Next"],
+        //   [nodeBefore, node.name, next],
+        // ]);
+        console.log(`${"  ".repeat(indentation)}entered: ${node.name}`);
+        indentation += 1;
+
+        if (node.name === "AlgoritmoDeclaration") {
+          const Identifier = node.node.getChild("Identifier");
+          // console.log(node.node.lastChild);
+          if (!Identifier) {
+            logError(
+              node,
+              "A palavra-chave Algoritmo deve ter um identificado após ser chamado, por exemplo: Algoritmo exemplo",
+              { to: 9 }
+            );
+          }
+        }
+
+        if (node.name === "EscrevaStatement") {
+          const StringLiteral = node.node.getChild("StringLiteral");
+          if (!StringLiteral) {
+            logError(
+              node,
+              'Para a utilização do Escreva é necessário ter um texto após ele entre aspas, desta forma: Escreva "Hello World!"'
+            );
+          }
+        }
+
+        // nodeBefore = node.name;
+      },
+      (node) => {
+        indentation -= 1;
+        console.log(`${"  ".repeat(indentation)}leave: ${node.name}`);
+      }
+    );
+
+  return diagnostics;
+});
+
 const parserWithMetadata = parser.configure({
   props: [
     styleTags({
@@ -105,6 +171,9 @@ const AlgorithmLanguage = new LanguageSupport(
       autocomplete: AutoComplete,
     },
   }),
+  {
+    extension: [Linter],
+  }
 );
 
 export { AlgorithmLanguage };
